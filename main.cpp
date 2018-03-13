39131ee393aa40792afddf9be73e8a9671bcdc1c
maxwellthefourth <maxwellthefourth@maxwellthefourth-VirtualBox>
2018-03-12 19:34:42 -0600

    fill broken, still needs scheduler

--- main.cpp
+++ main.cpp
@@ -7,20 +7,21 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include <MMU.h>
 #include "PageFrameAllocator.h"
 #include "ProcessTrace.h"
-
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     std::cerr << "Unable to open trace file. Exiting..." << std::endl;
     exit(1);
   }
+  std::string fileName = argv[1];
   
   mem::MMU memory(1024); // create an instance of the MMU class with 1024 (0x400) page frames
   PageFrameAllocator allocator(memory);
-  ProcessTrace trace(memory, allocator, argv[1]);
+  ProcessTrace trace(memory, allocator, fileName);
   trace.Execute();
   return 0;
 }
